60c44192f6ef2eabe77bd38bbc511fa02106e8c6
David L. Whitehurst <dwhitehurst@ms3-inc.com>
2021-11-14 19:36:57 -0500

    v0.1.26

--- resources/labdocs-clone-repo.sh
+++ resources/labdocs-clone-repo.sh
@@ -5,5 +5,12 @@ git clone https://github.com/dlwhitehurst/labdocs.git
 cd labdocs
 mvn clean package
 cd target/generated-docs
-echo "Abc123" | rlogin 192.168.1.20 -l david
-rcp -r * 192.168.1.20:/srv/nfs4/k8s/pvc-9a2fe61c-e5fb-4386-adf3-083caebece7d/
+
+# mount pvc directory using NFS
+apt-get update
+apt-get install -y nfs-common
+
+mkdir -p /mnt/pvc-9a2fe61c-e5fb-4386-adf3-083caebece7d
+mount 192.168.1.20:/srv/nfs4/k8s/pvc-9a2fe61c-e5fb-4386-adf3-083caebece7d /mnt/pvc-9a2fe61c-e5fb-4386-adf3-083caebece7d
+
+cp * /mnt/pvc-9a2fe61c-e5fb-4386-adf3-083caebece7d/
