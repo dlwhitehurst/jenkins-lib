1ea6cfca4bd52cb6014e1f362933b3d6dc35636a
David L. Whitehurst <dwhitehurst@ms3-inc.com>
2021-11-14 19:16:55 -0500

    v0.1.25

--- resources/labdocs-clone-repo.sh
+++ resources/labdocs-clone-repo.sh
@@ -5,4 +5,5 @@ git clone https://github.com/dlwhitehurst/labdocs.git
 cd labdocs
 mvn clean package
 cd target/generated-docs
-echo "Abc123" | sshpass scp -r * david@192.168.1.20:/srv/nfs4/k8s/pvc-9a2fe61c-e5fb-4386-adf3-083caebece7d/
+echo "Abc123" | rlogin 192.168.1.20 -l david
+rcp -r * 192.168.1.20:/srv/nfs4/k8s/pvc-9a2fe61c-e5fb-4386-adf3-083caebece7d/
