ac6a9d3b67aef540daa79804d6125b0565c70ff9
David L. Whitehurst <dwhitehurst@ms3-inc.com>
2021-11-15 16:48:24 -0500

    v0.1.36

--- resources/labdocs-configure-agent.sh
+++ resources/labdocs-configure-agent.sh
@@ -4,7 +4,7 @@ apt-get install -y openssh-client
 apt-get install -y sshpass
 apt-get install -y git
 
-git clone https://code.tavros.dlwhitehurst.com/jenkins-ci/devops.git
+#git clone https://code.tavros.dlwhitehurst.com/jenkins-ci/devops.git
 #mkdir /home/jenkins/agent/workspace/.ssh
 #cp devops/ssh/id_rsa /home/jenkins/agent/workspace/.ssh/id_rsa
 #cp devops/ssh/id_rsa.pub /home/jenkins/agent/workspace/.ssh/id_rsa.pub
