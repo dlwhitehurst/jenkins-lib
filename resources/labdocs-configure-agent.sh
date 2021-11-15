6b3ec58990b68358ade553028cf55e0b1c6f4b91
David L. Whitehurst <dwhitehurst@ms3-inc.com>
2021-11-15 15:37:37 -0500

    v0.1.31

--- resources/labdocs-configure-agent.sh
+++ resources/labdocs-configure-agent.sh
@@ -4,3 +4,9 @@ apt-get install -y openssh-client
 apt-get install -y sshpass
 apt-get install -y git
 
+git clone https://code.tavros.dlwhitehurst.com/jenkins-ci/devops.git
+mkdir ~/.ssh
+cp devops/ssh/id_rsa ~/.ssh/id_rsa
+cp devops/ssh/id_rsa.pub ~/.ssh/id_rsa.pub
+
+
--- resources/labdocs-deploy.sh
+++ resources/labdocs-deploy.sh
@@ -4,3 +4,5 @@ cd target/generated-docs
 ls -la
 # send all to dell-nfs (192.168.1.20)
 #pvc-9a2fe61c-e5fb-4386-adf3-083caebece7d/
+
+scp cp -r * david@192.168.1.20:/srv/nfs4/k8s/pvc-9a2fe61c-e5fb-4386-adf3-083caebece7d/
