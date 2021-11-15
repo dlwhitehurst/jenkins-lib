0e9a30043be7f9710d0343d3b5b500196ad3bd81
David L. Whitehurst <dwhitehurst@ms3-inc.com>
2021-11-14 20:32:54 -0500

    v0.1.27

--- resources/labdocs-clone-repo.sh
+++ resources/labdocs-clone-repo.sh
@@ -10,7 +10,10 @@ cd target/generated-docs
 apt-get update
 apt-get install -y nfs-common
 
-mkdir -p /mnt/pvc-9a2fe61c-e5fb-4386-adf3-083caebece7d
-mount 192.168.1.20:/srv/nfs4/k8s/pvc-9a2fe61c-e5fb-4386-adf3-083caebece7d /mnt/pvc-9a2fe61c-e5fb-4386-adf3-083caebece7d
+mkdir -p /mnt/k8s
+mount 192.168.1.20:/srv/nfs4/k8s
+mkdir -p /mnt/k8s/apache
 
-cp * /mnt/pvc-9a2fe61c-e5fb-4386-adf3-083caebece7d/
+cp -r * /mnt/k8s/apache/
+
+#pvc-9a2fe61c-e5fb-4386-adf3-083caebece7d/
