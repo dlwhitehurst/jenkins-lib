05a1df2c5bd3dad0c7fc21e51a484ca1d6c8db2e
David L. Whitehurst <dwhitehurst@ms3-inc.com>
2021-11-15 21:22:20 -0500

    v0.1.42

--- resources/labdocs-deploy.sh
+++ resources/labdocs-deploy.sh
@@ -1,15 +1,11 @@
 #!/usr/bin/env bash
 
-cd /home/jenkins/agent/workspace/labdocs/target/generated-docs
-ls -la
 # send all to dell-nfs (192.168.1.20)
 #pvc-9a2fe61c-e5fb-4386-adf3-083caebece7d/
-
-whoami
-echo $HOME
 
 # user is root here not david
 ssh-keygen -t rsa -f $HOME/.ssh/id_rsa <<< y
 echo "Abc123" | sshpass ssh-copy-id -o StrictHostKeyChecking=no david@192.168.1.20
-# scp -r * david@192.168.1.20:/srv/nfs4/k8s/pvc-9a2fe61c-e5fb-4386-adf3-083caebece7d/
-scp -r * david@192.168.1.20:/srv/nfs4/k8s/apache/
+cd labdocs/target/generated-docs
+scp -r * david@192.168.1.20:/srv/nfs4/k8s/pvc-9a2fe61c-e5fb-4386-adf3-083caebece7d/
+
