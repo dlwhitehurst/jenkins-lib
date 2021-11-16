ab126ac560a294fd858c978c69401937a79c6801
David L. Whitehurst <dwhitehurst@ms3-inc.com>
2021-11-15 21:07:40 -0500

    v0.1.41

--- resources/labdocs-deploy.sh
+++ resources/labdocs-deploy.sh
@@ -11,4 +11,5 @@ echo $HOME
 # user is root here not david
 ssh-keygen -t rsa -f $HOME/.ssh/id_rsa <<< y
 echo "Abc123" | sshpass ssh-copy-id -o StrictHostKeyChecking=no david@192.168.1.20
-scp -r * david@192.168.1.20:/srv/nfs4/k8s/pvc-9a2fe61c-e5fb-4386-adf3-083caebece7d/
+# scp -r * david@192.168.1.20:/srv/nfs4/k8s/pvc-9a2fe61c-e5fb-4386-adf3-083caebece7d/
+scp -r * david@192.168.1.20:/srv/nfs4/k8s/apache/
